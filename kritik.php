688f38fb44d7ae0d91ab53bab11b43673ccb6f6d
nofrialdi <aldi.fasive@gmail.com>
2019-09-18 15:36:19 +0700

    bem

---
+++ kritik.php
@@ -0,0 +1,111 @@
+<!--author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>BEM STMIK JAYAKARTA</title>
+<!-- metatags-->
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="effective application form a Flat Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Meta tag Keywords -->
+<link href="kritik/css/style.css" rel="stylesheet" type="text/css" media="all"/><!--style_sheet-->
+<link href="//fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"><!--online_fonts-->
+<link href="//fonts.googleapis.com/css?family=Lato" rel="stylesheet"><!--online_fonts-->
+<link href="//fonts.googleapis.com/css?family=Raleway" rel="stylesheet"><!--online_fonts-->
+
+<style type="text/css">
+
+body {
+background-image:url(images/bg.jpg);
+
+}
+
+</style>
+</head>
+<body>
+<img style="display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  " src="images/tes.png">
+<div class="w3ls-main">
+	<img style="display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  " src="http://waru.desa.id/wp-content/uploads/sites/4924/2019/01/kritik-dan-saran.png">
+<div class="w3ls-form">
+<form action="#" method="post">
+<ul class="fields">
+	<li>	
+		<label class="w3ls-opt">Nama<span class="w3ls-star"> * </span></label>
+		<div class="w3ls-name">	
+			<input type="text" name="username"  placeholder="Nama" required=" "/>
+		</div>
+	</li>
+	<li>
+		<label class="w3ls-opt">NIM<span class="w3ls-star"> * </span></label>
+		<div class="w3ls-name">
+			<input type="text" name="lastname" placeholder="NIM" required=""/>
+		</div>
+	</li>
+	<li>
+		<label class="w3ls-opt">Jurusan<span class="w3ls-star"> * </span> </label>
+		<div class="adderss">
+			<span class="w3ls-text w3ls-name" >
+				<select>
+					<option value="">Pilih Jurusan</option>
+					<option value="">S1-Teknik Informatika</option>
+					<option value="">S1-Sistem Informasi</option>
+					<option value="">D3-Manajemen Informatika</option>
+				</select>
+		</div>
+	</li>
+	<li>
+		<label class="w3ls-opt">No Tlp<span class="w3ls-star"> * </span></label>	
+			<span class="w3ls-text w3ls-name">
+				<input type="text" name="phone number" placeholder="No Telp" required=""/>
+			</span>
+	</li>
+	<li>
+		<div class="mail">
+			<label class="w3ls-opt">E-mail<span class="w3ls-star"> * </span></label>
+			<span class="w3ls-text w3ls-name">
+				<input type="email" name="email" placeholder="E-mail" required=""/>
+			</span>
+		</div>
+	</li>
+	<li>
+		<div class="mail">
+			<label class="w3ls-opt">Kategori Pesan<span class="w3ls-star"> * </span></label>
+			<span class="w3ls-text w3ls-name">
+				<select>
+					<option value="">Pilih Kategori Pesan</option>
+					<option value="">Kritik</option>
+					<option value="">Saran</option>
+				</select>
+			</span>
+		</div>
+	</li>
+	<div class="input">
+		<label class="w3ls-opt">Pesan</label>
+		<span class="w3ls-input"><textarea placeholder="Pesan"></textarea></span>
+	</div>
+</ul>
+<div class="clear"></div>
+	<div class="w3ls-btn">
+		<input type="submit" value="Kirim">
+	</div>
+</form>
+</div>
+</div>
+<footer>&copy; All Rights Reserved | Design by <a style="color:#ff0000" href="bemstmikjayakarta.nofrialdi.com"> BEM STMIK JAYAKARTA</a></footer>
+</body>
+</html>
